075f489c1c98fb508a8258c33590f10a48ef0d6f
kirestein <erik.proenca2011@gmail.com>
2021-06-11 13:23:27 -0300

    Manipulando o DOM no JS para mudar a impressão da lista

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,19 @@
+const criarTarefa = (evento) =>{
+
+    evento.preventDefault()
+
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+
+    const tarefa = document.querySelector('[data-task]');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    input.value = " ";
+}
+
 const novaTarefa = document.querySelector('[data-form-button]');
 
-// evento de click
-
-novaTarefa.addEventListener('click', () => {
-    console.log('fui clicado');
-})
+novaTarefa.addEventListener('click', criarTarefa);
 
