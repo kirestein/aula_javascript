53d2e3157fb49ec86b5f95ab467b3abba08797f2
kirestein <erik.proenca2011@gmail.com>
2021-06-11 13:56:27 -0300

    Finalizando o botão de concluir

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -24,8 +24,16 @@ novaTarefa.addEventListener('click', criarTarefa);
 const BotaoConclui = () => {
     const botaoConclui = document.createElement('button');
 
-    botaoConclui.addEventListener('click', () => {
-        console.log('fui clicado');
-    })
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+    botaoConclui.addEventListener('click', concluirTarefa)
     return botaoConclui;
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target;
+
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle('done');
 }
